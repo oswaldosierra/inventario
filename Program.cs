0e549559484723210ed1f48fdfa2a280b1df6af9
Oswaldo Sierra <oswaldox199@gmail.com>
2026-06-03 10:18:22 -0500

    Agregar archivos de configuración inicial del proyecto y estructura básica del sistema de inventario

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// ==================================================
+// SISTEMA DE INVENTARIO
+// Estado: Estructura profesional configurada
+// ==================================================
+
+
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+Console.WriteLine("==============================================");
+Console.WriteLine("SISTEMA DE INVENTARIO");
+Console.WriteLine("==============================================");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($"Versión: {version}");
+Console.WriteLine($".NET Versión: {Environment.Version}");
+Console.WriteLine("");
